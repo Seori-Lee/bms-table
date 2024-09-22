8382299cdd8e552e12b499c628b7ad6427be1218
Seori-Lee <49297010+Seori-Lee@users.noreply.github.com>
2024-09-22 14:10:06 +0900

    Update table.js

--- assets/js/table.js
+++ assets/js/table.js
@@ -161,7 +161,7 @@ const tableData = {
     if (row.url) {
       artistStr = `<a href='${row.url}' target='_blank'>${data || row.url}</a>`;
     } else {
-      return row.artist;
+      artistStr = row.artist;
     }
     if (row.url_pack) {
       if (row.name_pack) {
